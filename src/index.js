ce6ac6fdafafbb0a7d6e675ef3ffd53dc028575d
codetaha <taha.kachwala21@gmail.com>
2017-07-30 23:08:11 +0300

    added Webpack and its config with Sourcemaps

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by taha on 7/30/17.
+ */
+import './index.css';
+import numeral from 'numeral';
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`);
