f1c155d45629220e022f196a6f47d8908ef092f5
Taha Kachwala <taha.kachwala21@gmail.com>
2017-08-05 15:46:07 +0300

    Revert "Http calls"

--- src/api/baseUrl.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by taha on 7/31/17.
- */
-export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
-}
